05bec7671f7545098001df8db62018f55812445a
Jenyaza01 <jenyazagorulko@yandex.ua>
2019-02-21 13:29:02 +0200

    Upload project

---
+++ Wakeup/Wakeup/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Wakeup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "WakeupDetector";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timeToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            this.contextMenuStrip1.Text = "Close";
+            // 
+            // timeToolStripMenuItem
+            // 
+            this.timeToolStripMenuItem.Name = "timeToolStripMenuItem";
+            this.timeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.timeToolStripMenuItem.Text = "time";
+            this.timeToolStripMenuItem.Click += new System.EventHandler(this.timeToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 20000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(10, 10);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Leave += new System.EventHandler(this.Form1_Leave);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timeToolStripMenuItem;
+    }
+}
+
---
+++ Wakeup/Wakeup/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace Wakeup
+{
+    public partial class Form1 : Form
+    {
+        DateTime farTime;
+        DateTime lastTime;
+        DateTime currTime;
+        TimeSpan deltaTime;
+
+        Point farPos;
+        Point lastPos;
+        Point currPos;
+        double deltaPos;
+
+        string path = Application.StartupPath + "\\Wakeup.bat";
+        string param = "/c \"" + Application.StartupPath + "\\Wakeup.bat\"";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Hide();
+            timer1.Enabled = true; // init timer
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currTime = DateTime.Now;
+            lastTime = currTime;
+            currPos = Cursor.Position;
+            lastPos = currPos;
+            timer1.Enabled = false;
+            timer2.Enabled = true;
+            Update();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            this.Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Update();
+        }
+
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            notifyIcon1.Dispose();
+        }
+
+
+        new void Update()
+        {
+            farTime = lastTime;
+            lastTime = currTime;
+            currTime = DateTime.Now;
+            deltaTime = lastTime - farTime;
+
+            farPos = lastPos;
+            lastPos = currPos;
+            currPos = Cursor.Position;
+            deltaPos = dist2(lastPos, farPos);
+
+            if (deltaTime.TotalSeconds > 42)
+            {
+                if( deltaPos < 2)
+                    System.Diagnostics.Process.Start("cmd.exe", param);
+            }
+            timeToolStripMenuItem.Text = String.Format("{0:f}s, {1:f1} px2", deltaTime.TotalSeconds, deltaPos);
+
+        }
+
+        private double dist2(Point p1, Point p2)
+        {
+            int dx = p2.X - p1.X;
+            int dy = p2.Y - p1.Y;
+            return (dx * dx) + (dy * dy);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                FileStream f = File.Create(path);
+                f.Close();
+            }
+        }
+
+        private void timeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd.exe", param);
+        }
+    }
+}
